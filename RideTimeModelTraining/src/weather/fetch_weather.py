ed0b273c1306928d8fbddad4d5668425d4b95b65
Raphael Guye <raphael.guye@gmail.com>
2025-07-27 22:22:52 +0200

    public func fetch_weather and GMT timezone

--- RideTimeModelTraining/src/weather/fetch_weather.py
+++ RideTimeModelTraining/src/weather/fetch_weather.py
@@ -3,14 +3,14 @@ import pandas as pd
 from urllib.parse import urlencode
 import os
 
-def fetch_weather_data(date, location):
+def _fetch_weather_data(date, location):
     api_url = "https://historical-forecast-api.open-meteo.com/v1/forecast"
     params = {
         "latitude": location['lat'],
         "longitude": location['lon'],
         "start_date": date,
         "end_date": date,
-        "timezone": "Europe/Zurich",
+        "timezone": "GMT",
         "hourly": "temperature_2m,wind_speed_10m,precipitation",
     }
 
@@ -19,16 +19,13 @@ def fetch_weather_data(date, location):
     print(f"Requesting weather data from: {full_url}")
 
     response = requests.get(api_url, params=params)
-    # print(response.json())
-
     if response.status_code == 200:
         weather_data = response.json()
         return weather_data
     else:
         raise Exception(f"Error fetching weather data: {response.status_code}")
 
-def process_weather_data(weather_data, target_hour=None):
-    # Extract relevant weather information
+def _process_weather_data(weather_data, target_hour=None):
     temperature = weather_data['hourly']['temperature_2m']
     wind_speed = weather_data['hourly']['wind_speed_10m']
     rain = [precip > 0 for precip in weather_data['hourly']['precipitation']]
@@ -38,20 +35,21 @@ def process_weather_data(weather_data, target_hour=None):
         if target_hour is None or t.endswith(f"T{target_hour}"):
             return {
                 'time': t,
+                'timezone': weather_data.get('timezone'),
                 'temperature': temp,
                 'wind_speed': ws,
                 'rain': r
             }
     return None
 
-def main():
-    # Example usage
-    date = "2025-01-01"
-    location = {'lat': 46.9910, 'lon': 6.9293}
-    target_hour = "14:00"  # Set your desired hour here
-    weather_data = fetch_weather_data(date, location)
-    processed_data = process_weather_data(weather_data, target_hour=target_hour)
-    print(processed_data)
+def fetch_weather(date, hour, location):
+    weather_data = _fetch_weather_data(date, location)
+    return _process_weather_data(weather_data, target_hour=hour)
 
+# Example usage for testing
 if __name__ == "__main__":
-    main()
+    date = "2025-01-01"
+    hour = "12:00"
+    location = {'lat': 46.9910, 'lon': 6.9293}
+    result = fetch_weather(date, hour, location)
+    print(result)
